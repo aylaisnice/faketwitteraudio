d5a075301b66870196d616c9c4a1fab0ac6bf1bf
homosexuayla <76013118+homosexuayla@users.noreply.github.com>
2023-11-02 16:58:37 -0300

    bigger speaker and variable resolution

--- lala.sh
+++ lala.sh
@@ -3,13 +3,16 @@
 AUDIO_FILE=$1
 GIF_PFP="";PFP=pfp.jpg
 #GIF_PFP="-stream_loop -1";PFP=3ds.gif
-PFP_SIZE=200
+RES=1
+PFP_SIZE=$((200*$RES))
+TEXT_SIZE=$((24*$RES))
+VIDEO_WIDTH=$((640*$RES))
+VIDEO_HEIGHT=$((360*$RES))
 AUDIO_SECS=$(ffprobe -i $1 -show_entries format=duration -v quiet -of csv="p=0")
 COLOR="#ff80b3"
 TEXT_COLOR="#ffffffbb"
-TEXT_SIZE=24
 ffmpeg -y -i $AUDIO_FILE \
-  -f lavfi -i color=c=$COLOR:s=640x360 \
+  -f lavfi -i color=c=$COLOR:s=$VIDEO_WIDTH"x"$VIDEO_HEIGHT \
   $GIF_PFP -i $PFP -i circle.png \
   -i speaker.png \
   -filter_complex \
@@ -19,7 +22,8 @@ ffmpeg -y -i $AUDIO_FILE \
     [1]drawtext=text='⬤':fontfile=symbols.ttf:x=(W-text_w)/2:y=(H-text_h)/2:fontsize=$PFP_SIZE+sin(t*6)*30:fontcolor=$TEXT_COLOR[bg];\
     [bg][masked_profile]overlay=x=(W-w)/2:y=(H-h)/2[bg];\
     [bg]drawtext=text='%{eif\:floor(($AUDIO_SECS-t)/60)\:d\:1}\:%{eif\:mod(($AUDIO_SECS-t)\,60)\:d\:2}'\
-    :fontfile=Chirp-Regular.woff2:x=20:y=H-35:fontsize=$TEXT_SIZE:fontcolor=$TEXT_COLOR[bg];\
-    [bg][4:v]overlay=x=78:y=H-37[bg];\
-    [bg]drawtext=text='Voice':fontfile=Chirp-Bold.woff2:x=(W-text_w)-20:y=H-35:fontsize=$TEXT_SIZE:fontcolor=$TEXT_COLOR;"\
+    :fontfile=Chirp-Regular.woff2:x=20*$RES:y=H-35*$RES:fontsize=$TEXT_SIZE:fontcolor=$TEXT_COLOR[bg];\
+    [4:v]scale=$TEXT_SIZE:$TEXT_SIZE[speaker];\
+    [bg][speaker]overlay=x=78*$RES:y=H-37*$RES[bg];\
+    [bg]drawtext=text='Voice':fontfile=Chirp-Bold.woff2:x=(W-text_w)-20*$RES:y=H-35*$RES:fontsize=$TEXT_SIZE:fontcolor=$TEXT_COLOR;"\
   -c:v libx264 -c:a aac -strict experimental -t $AUDIO_SECS output.mp4
